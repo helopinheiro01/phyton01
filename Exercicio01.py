5b75757859a2b706d872a945e0f2fcba5408d38f
helopinheiro01 <heloisa.pinheiro@fatec.sp.gov.br>
2024-08-16 21:37:49 -0300

    exercicio1

--- Exercicio01.py
+++ Exercicio01.py
@@ -1,8 +1,19 @@
 import os
+os.system("cls")
 
-for i in range()
-    while True:
-    try:
-        n = int(input("Entre com um numero: "))
-    except ValueError:
-        print("O valor informado não é um numero")
+n = input("Digite o nome de um produto: ")
+valor = int(input("Digite o valor do produto:"))
+if (valor >= 50 and valor <200):
+    desconto = valor - valor * 0.05
+if(valor >= 200 and valor <500):
+    desconto = valor - valor * 0.06
+if (valor >= 500 and valor <1000):
+    desconto = valor - valor * 0.07
+if (valor >=1000):
+    desconto = valor - valor * 0.08
+if (valor <= 0):
+    desconto = 0
+print(f"Nome: {n}")
+print(f"Valor: {valor}")
+print(f"Desconto: {desconto}")
+
