978356e91997ef1f599875be975b827f12b4b97d
helopinheiro01 <heloisa.pinheiro@fatec.sp.gov.br>
2024-08-30 21:46:00 -0300

    exercicios aula 30/08

---
+++ exercicio-funcao.py
@@ -0,0 +1,23 @@
+import os
+
+def limpar_tela():
+    os.system('cls')
+
+def ler_numero():
+        try:
+            return int(input("Informe um valor:"))
+        except:
+            print ("Valor Invalido")
+            return ler_numero()
+
+def soma(n1,n2):
+    return n1+n2
+    
+def main():
+    limpar_tela()
+    n1 = ler_numero()
+    n2 = ler_numero()
+    print(soma(n1,n2))
+
+main()
+
